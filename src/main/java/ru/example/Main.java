2c803576ae13dffd78ee27d09956b4f45e0ecf1d
arsenevaea <arsenjeva.ea@gmail.com>
2024-08-04 18:15:06 +0700

    variants for Time

--- src/main/java/ru/example/Main.java
+++ src/main/java/ru/example/Main.java
@@ -1,5 +1,8 @@
 package ru.example;
 
+import ru.example.util.TimeFormatter;
+
+import java.sql.Time;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
@@ -10,9 +13,21 @@ public class Main {
         String startTimeString = "09:00";
         int timeZone = 3;
 
+        LocalTime startTime = parseFromString(startTimeString, timeZone);
+
+        System.out.println("Second from String: " + startTime);
+
+        Integer intSecond = 2024;
+
+        String secondFromInt =  TimeFormatter.formatSeconds(intSecond);
+        System.out.println("Second from int: " + secondFromInt);
+
+        Time timeSecond = TimeFormatter.stringToTime(startTimeString);
+        System.out.println("Time from String: " + timeSecond);
+    }
+
+    private static LocalTime parseFromString(String startTimeString, int timeZone) {
         LocalTime startTime = LocalTime.parse(startTimeString, DateTimeFormatter.ofPattern("HH:mm")).minusHours(timeZone);
-
-        System.out.println(startTime);
-
+        return startTime;
     }
 }
